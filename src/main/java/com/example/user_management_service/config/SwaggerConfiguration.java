498ae6021d34ed54a03420089ee302a4493a88ac
= <=>
2025-01-11 04:49:07 +0500

    swagger ip change

--- src/main/java/com/example/user_management_service/config/SwaggerConfiguration.java
+++ src/main/java/com/example/user_management_service/config/SwaggerConfiguration.java
@@ -30,8 +30,8 @@ public class SwaggerConfiguration implements WebMvcConfigurer{
                         .termsOfService("http://swagger.io/terms/")
                         .license(new License().name("Apache License").url("https://apache.co")))
                 .servers(List.of(
-                        new Server().url("http://localhost:8080").description("Falcon server")
-//                        new Server().url("http://209.38.109.22:8080").description("Falcon server")
+//                        new Server().url("http://localhost:8080").description("Falcon server")
+                        new Server().url("http://209.38.109.22:8080").description("Falcon server")
                 ))
                 .security(List.of(new SecurityRequirement().addList("bearer-key"))); // Global security requirement
     }
