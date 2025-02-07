a19550bd5113d571395a96d4e1771d0232483d11
= <=>
2025-02-07 16:20:28 +0500

    bug fixes

---
+++ src/main/java/com/example/user_management_service/model/dto/DoctorRecipeStatsDTO.java
@@ -0,0 +1,23 @@
+package com.example.user_management_service.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * Date-2/7/2025
+ * By Sardor Tokhirov
+ * Time-9:50 AM (GMT+5)
+ */
+
+
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class DoctorRecipeStatsDTO {
+//    private UUID doctorId;
+//    private Integer recipesCreatedThisMonth;
+//    private Integer averageRecipesPerMonth;
+//}
