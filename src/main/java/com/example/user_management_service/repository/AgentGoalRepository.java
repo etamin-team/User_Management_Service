0c057a1f9bf8dfbe443ca07814204b819fd4902d
= <=>
2025-05-29 06:07:27 +0500

    problem solving

--- src/main/java/com/example/user_management_service/repository/AgentGoalRepository.java
+++ src/main/java/com/example/user_management_service/repository/AgentGoalRepository.java
@@ -16,6 +16,4 @@ public interface AgentGoalRepository extends JpaRepository<AgentGoal, Long> {
     @Query("SELECT ac FROM AgentGoal ac WHERE ac.medAgent.userId = :medAgentId "+
             "AND ac.status = 'APPROVED' ")
     Optional<AgentGoal> getGoalsByMedAgentUserId(@Param("medAgentId") UUID medAgentId);
-    @Query("SELECT a FROM AgentGoal a WHERE a. = :contractId")
-    Optional<AgentGoal> findByContractId(@Param("contractId") Long contractId);
 }
